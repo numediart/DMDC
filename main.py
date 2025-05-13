3f07124bd5cf1a90a28859835f831de2fff2f194
gaspardcrepin <gaspard.crepin@student.junia.com>
2025-05-13 14:39:57 +0200

    Added MFCC to Pipeline

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from whoIsSpeaking import run_diarization
 from Filtering.filter import process_video
+from extractMFCC import extractAndSaveMFCC
+import librosa
+import os
 
 def main_batch(video_list_file='videoV0.txt'):
     with open(video_list_file, 'r') as f:
@@ -9,6 +12,11 @@ def main_batch(video_list_file='videoV0.txt'):
         try:
             process_video(url, idx)
             run_diarization(f"{idx}_video.wav")
+
+            wav_path = os.path.abspath(f'./V0DataSet/wav/{idx}_video.wav')
+            signal, sr = librosa.load(wav_path)
+            os.makedirs(os.path.dirname("V0DataSet/mffc/"), exist_ok=True)
+            extractAndSaveMFCC(signal, './V0DataSet/mffc', f'{idx}_video')
         except Exception as e:
             print(f"Erreur lors du traitement de la vidéo {url} : {e}")
 
