08968600c6628f217660f06ebef3c3e9a5097539
HugoMany <hugo.many@student.junia.com>
2025-05-13 11:52:25 +0200

    chore: Refactor extractAndSaveMFCC function and add docstring

--- extractMFCC.py
+++ extractMFCC.py
@@ -5,14 +5,28 @@ import matplotlib.pyplot as plt
 import os
 
 def extractAndSaveMFCC(signal, folder, fileName):
-        print(signal)
+        """
+        Extracts Mel-frequency cepstral coefficients (MFCC) from a given audio signal and saves the corresponding spectrogram plot as a PNG file.
+        Parameters:
+        - signal: numpy.ndarray
+            The input audio signal.
+        - folder: str
+            The folder path where the PNG file will be saved.
+        - fileName: str
+            The name of the PNG file (without extension).
+        Returns:
+        - mfcc: numpy.ndarray
+            The extracted MFCC features.
+        """
+        
         mfcc = librosa.feature.mfcc(y=signal)
         librosa.display.specshow(mfcc)
         plt.savefig(folder + '/' + fileName + '.png')
         return mfcc
    
 
-# Get the absolute path of the WAV file
+
+# Example usage
 wav_path = os.path.abspath('./V0DataSet/wav/1_video.wav')
 signal, sr = librosa.load(wav_path)
 
