5a2e38577f3f15ae35c8c63e48dbc63e79f473cd
HugoMany <90179566+HugoMany@users.noreply.github.com>
2025-05-20 09:41:16 +0200

    Enhance audio transcription by processing all WAV files in the input directory and generating corresponding transcripts

--- Whisper/mainWhisper.py
+++ Whisper/mainWhisper.py
@@ -1,7 +1,9 @@
 import os
 from transcriptFromAudio import transcriptFromAudio
 
-input_path = os.path.join(os.path.dirname(__file__), "input", "testclip_001.wav")
 output_path = os.path.join(os.path.dirname(__file__), "output")
 
-transcriptFromAudio(audiofile=input_path, outputFolder=output_path)
+for filename in os.listdir(os.path.join(os.path.dirname(__file__), "input")):
+    if filename.endswith(".wav"):
+        input_file = os.path.join(os.path.dirname(__file__), "input", filename)
+        transcriptFromAudio(audiofile=input_file, outputFolder=output_path)
