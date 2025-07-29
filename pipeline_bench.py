ce91527e7500a3ae03764d3be531549c4c3f3295
HugoMany <90179566+HugoMany@users.noreply.github.com>
2025-07-29 16:50:46 +0200

    25 26 27

--- pipeline_bench.py
+++ pipeline_bench.py
@@ -16,12 +16,12 @@ warnings.filterwarnings("ignore", message=".*speechbrain.pretrained.*was depreca
 
 # Constants 
 
-DATASET_FOLDER="V0.11DataSet"
+DATASET_FOLDER="V0.12DataSet"
 VIDEO_TEXT_FILE="./VideoList/videos_benchmarkDMDCW11.txt"
 WINDOWING_SIZE_FRAME=128
 WINDOWING_SIZE_STEP=32
-START_VIDEO=22
-END_VIDEO=24
+START_VIDEO=27
+END_VIDEO=28
 
 
 
